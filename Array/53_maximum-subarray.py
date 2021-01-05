83f471f07c5761564d37e715d60c841393916a12
LabPC <249912281@qq.com>
2021-01-05 14:17:06 +0800

    +53

---
+++ Array/53_maximum-subarray.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# @FileName  :53_maximum-subarray.py
+# @Time      :2020/12/28 22:08
+# @Author    :Haozr
+from typing import List
+
+
+# 解法1：
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        pass
+if __name__ == '__main__':
+    pass
